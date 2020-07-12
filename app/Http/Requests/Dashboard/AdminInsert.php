bdfed0b48d26a2ae359e1f4e281a763c934f833d
isheikh-dev <ishiekh-dev>
2020-07-13 00:05:10 +0200

    convert client update , admin insert - update  validation to be formRequest

---
+++ app/Http/Requests/Dashboard/AdminInsert.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Dashboard;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminInsert extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'password'=>'required',
+            'email'=>'required|email',
+            'admin_type'=>'required'
+        ];
+    }
+}
