49bf6b81abff870958be6bbb0fda13d449955441
Mohammed Alama <mohammedalsayed9688@gmail.com>
2020-07-21 19:53:30 +0200

    add auth interface to admin model

--- app/Admin.php
+++ app/Admin.php
@@ -3,34 +3,53 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    public function branches(){
+    protected $fillable = [
+        'username', 'email', 'password', 'super_admin',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function branches()
+    {
         return $this->hasMany(Branch::class);
     }
 
-    public function stores(){
+    public function stores()
+    {
         return $this->hasMany(Store::class);
     }
 
-    public function store_transfers(){
+    public function store_transfers()
+    {
         return $this->hasMany(Store_transfer::class);
     }
 
-    public function buy_orders(){
+    public function buy_orders()
+    {
         return $this->hasMany(Buy_order::class);
     }
 
-    public function sell_orders(){
+    public function sell_orders()
+    {
         return $this->hasMany(Sell_order::class);
     }
 
-    public function income_out_operations(){
+    public function income_out_operations()
+    {
         return $this->hasMany(Income_out_operation::class);
     }
 
-    public function branch(){
+    public function branch()
+    {
         return $this->hasOne(Branch::class);
     }
 }
